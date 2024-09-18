edc4a2b750dafffc8fad443287c1199f60e3d9ca
Anuska-1Dutta <145000591+Anuska-1Dutta@users.noreply.github.com>
2024-09-18 16:26:21 +0530

    sum.java

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class user{
+  public static void main(string args[])
+  {
+    Scanner sc= new Scanner(System.in);
+    System.out.println("enter 1st number");
+    int a,b,c;
+    a=sc.nextInt();
+    b=sc.nextInt();
+    c= a + b;
+    System.out.println("sum of two value"+c);
+  }
+}
