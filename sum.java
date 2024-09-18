e851ffb4b1ab11eb3fe9d61ecf05f953ca9d3c86
Anuska-1Dutta <145000591+Anuska-1Dutta@users.noreply.github.com>
2024-09-18 16:29:25 +0530

    sum.java

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 import java.util.*;
-class user{
-  public static void main(string args[])
+class User{
+  public static void main(String args[])
   {
     Scanner sc= new Scanner(System.in);
     System.out.println("enter 1st number");
